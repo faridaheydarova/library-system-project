9824b7eb313fb6ad70d027a5b3f99c6a5787ac38
farida2498 <125817327+faridaheydarova@users.noreply.github.com>
2023-08-24 08:52:24 -0700

    1-ci mərhələ
    spring proyekti yaradıldı

---
+++ library-system-farida/src/main/java/az/developia/librarysystemfarida/LibrarySystemFaridaApplication.java
@@ -0,0 +1,13 @@
+package az.developia.librarysystemfarida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemFaridaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemFaridaApplication.class, args);
+	}
+
+}
